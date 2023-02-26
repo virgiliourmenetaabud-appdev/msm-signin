c600ef8ebff543c36c153b1e4e667e123fd0eb91
virgiliourm <vurmenet@chicagobooth.edu>
2023-02-26 21:00:44 +0000

    73

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,9 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to :director
+  has_many :characters
+
+  has_many :cast, :through => :characters, :source => :actor
+  validates(:title, { :presence => true })
 end
